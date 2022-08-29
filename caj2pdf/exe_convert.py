a5fba65931a2e9bd80a9373ebc7d2f43b952e371
zombie110year <zombie110year@outlook.com>
2022-08-29 15:10:10 +0800

    feat(win): show program doesn't block

--- caj2pdf/exe_convert.py
+++ caj2pdf/exe_convert.py
@@ -4,6 +4,7 @@
 """
 import pathlib
 import sys
+import threading
 
 from .cajparser import CAJParser
 from .exceptions import Caj2PdfException
@@ -28,6 +29,36 @@ def app():
 
     inputfile = str(cajfilepath)
     outputfile = f"{inputfile}.pdf"
+    task = threading.Thread(
+        group=None, target=convert_caj, args=(inputfile, outputfile)
+    )
+    alive = AliveStatus()
+    task.start()
+    alive.start()
+    task.join()
+    alive.finish()
+    alive.join()
 
+
+def convert_caj(inputfile, outputfile):
     caj = CAJParser(inputfile)
     caj.convert(outputfile)
+
+
+class AliveStatus(threading.Thread):
+    def __init__(self):
+        super().__init__(group=None)
+        self.finished = False
+
+    def run(self):
+        from time import sleep
+
+        status = ("正在转换 <<<<<<", "正在转换 >>>>>>")
+        i = 0b0
+        while not self.finished:
+            print(status[i & 0b1], end="\r", file=sys.stderr)
+            i ^= 0b1
+            sleep(0.2)
+
+    def finish(self):
+        self.finished = True
